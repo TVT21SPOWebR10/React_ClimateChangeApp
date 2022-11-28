3ce114e758a6b762a0c6457f43a642737ef22d40
TuukkaHuru <t1hutu02@students.oamk.fi>
2022-11-28 19:13:26 +0200

    v3 ja v4 melkein

--- client/src/components/N1.js
+++ client/src/components/N1.js
@@ -4,6 +4,8 @@ import V1 from './charts/V1';
 import V7 from './charts/V7';
 import V5 from './charts/V5';
 import V6 from './charts/V6';
+import V3 from './charts/V3';
+
 
   export default function N1() {
 
@@ -11,6 +13,7 @@ import V6 from './charts/V6';
         <>
         <Navbar />
         <V1 /> <br/>
+        <V3 /> <br/>
         <V5 /> <br/>
         <V6 /> <br/>
         <V7 /> <br/>
